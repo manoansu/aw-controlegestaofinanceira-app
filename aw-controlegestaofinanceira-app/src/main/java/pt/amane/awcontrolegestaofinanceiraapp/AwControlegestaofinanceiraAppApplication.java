75588cb845f20af215f36f7615f3396df5142976
Ansumane Mané <manoansu@gmail.com>
2022-02-27 09:43:48 +0000

    startted project and category class is implemented

---
+++ aw-controlegestaofinanceira-app/src/main/java/pt/amane/awcontrolegestaofinanceiraapp/AwControlegestaofinanceiraAppApplication.java
@@ -0,0 +1,13 @@
+package pt.amane.awcontrolegestaofinanceiraapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwControlegestaofinanceiraAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwControlegestaofinanceiraAppApplication.class, args);
+	}
+
+}
---
+++ aw-controlegestaofinanceira-app/src/test/java/pt/amane/awcontrolegestaofinanceiraapp/AwControlegestaofinanceiraAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.amane.awcontrolegestaofinanceiraapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwControlegestaofinanceiraAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
